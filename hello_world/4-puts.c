60db5675c358c4b618b258fa91fa0939785ca751
a <a@gmail.com>
2024-06-17 09:08:36 +0000

    t4 hello world

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,7 @@
-int main (void)
+#include <stidio.h>
+
+int main(void)
 	{
-		printf(""Programming is like building a multilingual puzzle\n");
-		return0;
+		printf("Programming is like building a multilingual puzzle\n");
+		return (0);
 	}

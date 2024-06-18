47b0796b888d92ca0c291e8863b90207820fb2c3
a <a@gmail.com>
2024-06-18 08:55:05 +0000

    t0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -21,15 +21,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf(n, "%d is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf(n, "%d is zero\n");
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf(n, "%d is negative\n");
+		printf("%d is negative\n", n);
 	}
 
 

9ca7006843e92b285ab3c1a8cdb1698400fd65ae
a <a@>
2024-06-19 07:33:05 +0000

    t7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main -Entry point
+ *
+ *
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	 int ch;
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+	putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}

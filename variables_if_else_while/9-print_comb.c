8fad4775fa44e44d49a0b5ea4826b6aec4c58da7
a <a@>
2024-06-19 07:52:31 +0000

    t9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main -Entry point
+ *
+ *
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	 char ch;
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	putchar(ch);
+	putchar(',');
+	}
+
+	putchar('\n');
+	return (0);
+}

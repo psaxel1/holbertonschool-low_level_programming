1b8d6b07c93e5106d05fac94f5ec03b88482d544
a <a@>
2024-06-20 20:26:19 +0000

    t4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower -check if char is lower case is it return 1
+ *
+ * @c: is char
+ *
+ * Return: 1 if lower, 0 if else
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	return (0);
+}

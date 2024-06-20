891d417f658da58f3a340177fbe6056afab90af5
a <a@>
2024-06-20 20:13:04 +0000

    t3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
+/**
+ * _islower -check if char is lower case is it return 1
+ *
+ * @c: is char
+ *
+ * Return: 1 if lower, 0 if else
+ */
+
 int _islower(int c)
 {
-	if (islower (c))
+	if (islower(c))
 	{
-	       	return (1);
+		return (1);
 	}
 	return (0);
 }

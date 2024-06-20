f7f559abf3352a69e44814d3fabb5e231f1c631e
a <a@>
2024-06-20 11:35:44 +0000

    t0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 /**
  *main -description here
  *
  *
  * Return: always 0
  */
-
 int main(void)
 {
-	printf("_printchar");
-
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
 	return (0);
 }

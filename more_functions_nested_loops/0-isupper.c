4ed33107cc0deaeab637f9476c3c2a2b499f9d87
a <a@>
2024-06-24 09:27:11 +0000

    t0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper -check if char is lower case is it return 1
+ *
+ * @c: is char
+ *
+ * Return: 1 if upper, 0 if else
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(void);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _isupper(int c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int x);
+int print_last_digit(int n);
+void jack_bauer(void);
+
+#endif

aca92d11c8c7926b33b935e433e324e3817af063
a <a@>
2024-06-24 11:43:56 +0000

    t1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -9,17 +9,12 @@
 
 int _isdigit(int c)
 {
-	if (c <= 9)
+	if (c <= '9')
+	{
+		return (1);
+	}
+	else
 	{
 		return (0);
 	}
-	else if ( c < 0)
-	{
-		return (0);
-	}
-
-	if (c >= 10)
-		return (0);
-
-	return (1);
 }

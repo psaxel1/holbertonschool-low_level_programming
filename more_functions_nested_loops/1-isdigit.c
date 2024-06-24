e5dc3aa5390da78ebdeafda00039f973980cbb24
a <a@>
2024-06-24 11:51:51 +0000

    t1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -11,23 +11,23 @@ int _isdigit(int c)
 {
 	if (c == '0')
 		return (1);
-	else if (c == 1)
+	else if (c == '1')
 		return (1);
-	else if (c == 2)
+	else if (c == '2')
 		return (1);
-	else if (c == 3)
+	else if (c == '3')
 		return (1);
-	else if (c == 4)
+	else if (c == '4')
 		return (1);
-	else if (c == 5)
+	else if (c == '5')
 		return (1);
-	else if (c == 6)
+	else if (c == '6')
 		return (1);
-	else if (c == 7)
+	else if (c == '7')
 		return (1);
-	else if (c == 8)
+	else if (c == '8')
 		return (1);
-	else if (c == 9)
+	else if (c == '9')
 		return (1);
 
 	return (0);

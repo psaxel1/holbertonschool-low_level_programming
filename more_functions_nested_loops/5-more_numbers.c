4c4c59709f49bfd316fe03c0dc4f6a9cd5f4e6d2
a <a@>
2024-06-24 13:40:03 +0000

    t5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * more_numbers -print 1 to 14
+ *
+ */
+void more_numbers(void)
+{
+	int t, num;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+				putchar('0' + num / 10);
+
+			putchar('0' + num % 10);
+		}
+
+		putchar('\n');
+	}
+}

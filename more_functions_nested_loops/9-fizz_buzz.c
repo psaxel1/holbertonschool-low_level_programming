c6ba3414fd57ba0e4da6c83086e6c412fef0685f
a <a@>
2024-06-25 08:55:24 +0000

    t6

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print fizz buzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num <= 9)
+		{
+			putchar('0' + num % 10);
+			putchar(' ');
+		}
+		else
+		{
+			putchar ('0' + num / 10);
+			putchar ('0' + num % 10);
+			putchar (' ');
+		}
+	}
+	return (0);
+}

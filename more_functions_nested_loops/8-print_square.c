fcceb13565c6aa0d6b1af4a8d5a2a1076970744a
a <a@>
2024-06-25 08:15:29 +0000

    t8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square -function to print a square
+ * @size: number
+ */
+
+void print_square(int size)
+{
+	int idx1, idx2;
+
+	for (idx1 = 0; idx1 < size; idx1++)
+	{
+		for (idx2 = 0; idx2 < size; idx2++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}

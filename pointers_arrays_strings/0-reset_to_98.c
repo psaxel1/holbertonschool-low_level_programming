8f912af57bed526637c8f46f546cd92482e38421
a <a@>
2024-06-26 08:28:20 +0000

    t0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 -set poiteur of n to 98
+ * @n: poiteur of n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(void);
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif

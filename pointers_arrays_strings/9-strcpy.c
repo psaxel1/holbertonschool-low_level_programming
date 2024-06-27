c10dff8fb574e31ce0cfd4b3673c9e410283e912
a <a@>
2024-06-27 15:01:17 +0000

    t9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcpy -copie source to dest
+ *@dest: destination
+ *@src: source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+    return dest;
+}

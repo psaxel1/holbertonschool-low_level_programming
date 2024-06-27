28fc9b31daf867d77f36ca1942edd7709fa9fe86
a <a@>
2024-06-27 10:13:41 +0000

    t8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,26 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_array - print all arry selec
+ * @a: array
+ * @n: number of array to get print
+ */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n - 1; i++)
+	n = n - 1;
+	for (i = 0; i <= n; i++)
 	{
-		if ( i != n-1)
+		if (n <= 0)
+		{
+			printf(" \n");
+		}
+		else if (i != n)
 		{
 		printf("%d, ", a[i]);
 		}
-		else 
+		else
 		{
 			printf("%d\n", a[i]);
 		}

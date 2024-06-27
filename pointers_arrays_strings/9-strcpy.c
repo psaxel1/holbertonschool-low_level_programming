745bd0e620fa125707181d78cd673ae3fb8b83e2
a <a@>
2024-06-27 16:11:13 +0000

    t9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,14 +3,15 @@
  * _strcpy -copie source to dest
  *@dest: destination
  *@src: source
+ *Return: temp
  */
 char *_strcpy(char *dest, char *src)
 {
 	char *temp = dest;
 
-	while(*src)
+	while (*src)
 		*dest++ = *src++;
 	*dest = '\0';
 
-	return (temp);	
+	return (temp);
 }

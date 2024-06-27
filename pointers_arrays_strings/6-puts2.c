2999e15fefb9e179c611cdefd34ac14092f521a2
a <a@>
2024-06-27 09:00:47 +0000

    t6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main .h"
+/**
+ * puts2 -print 0 2 4 6 8 10 ...
+ *@str: array
+ */
+void puts2(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar (*str);
+		str++;
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
 #include <string.h>
 
 int main(void);
+void puts2(char *str);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);

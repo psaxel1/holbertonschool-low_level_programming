70cef891487b9a5963d0748e7562a42a4f571b10
a <a@>
2024-06-27 10:20:29 +0000

    t9

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,16 @@ void print_array(int *a, int n)
 	int i;
 
 	n = n - 1;
+
+	if (n <= 0)
+	{
+		printf(" \n");
+	}
+	else
+	{
 	for (i = 0; i <= n; i++)
 	{
-		if (n <= 0)
-		{
-			printf(" \n");
-		}
-		else if (i != n)
+		if (i != n)
 		{
 		printf("%d, ", a[i]);
 		}
@@ -24,5 +27,6 @@ void print_array(int *a, int n)
 		{
 			printf("%d\n", a[i]);
 		}
+	}
 	}
 }

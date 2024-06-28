803d5bd9c49da4bfc108c0e2e764f2145dce944a
a <a@>
2024-06-28 10:08:01 +0000

    t0t2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
 char *_strcat(char *dest, char *src)
 {
 	char *temp = dest;
-	
+
 	char *test = temp;
 
 	int i;
--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
- *
+ * _strncpy -copie string to dest from source
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[i] = src[i];
-    for ( ; i < n; i++)
-        dest[i] = '\0';
-    return dest;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }

797e1186271609f6f29402f225d814a942991f2f
a <a@>
2024-06-28 08:46:59 +0000

    t2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,19 @@
 #include "main.h"
-
-char *_strncat(char *dest, const char *src, int n)
+/**
+ * _strncat -description
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = strlen(dest);
-    int i;
-    for (i = 0 ; i < n && src[i] != '\0' ; i++)
-        dest[dest_len + i] = src[i];
-    dest[dest_len + i] = '\0';
-    return dest;
+	int dest_len = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
 
